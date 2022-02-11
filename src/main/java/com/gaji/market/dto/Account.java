224325cd07a0362d477c4b2afe370436496cee3b
gajeong <rlarwkjd127@gmail.com>
2022-02-11 18:29:11 +0900

    mysql 연결 확인

--- src/main/java/com/gaji/market/dto/Account.java
+++ src/main/java/com/gaji/market/dto/Account.java
@@ -26,24 +26,7 @@ public class Account {
 	// 파라미터 검증, NULL이면 오류 발생
 	@NotNull(message = "id는 필수 값입니다.")
     private String AccountId;
-	@PositiveOrZero
-	private String tpAccount;
+
 	private String AccountNm;
-	private String Password;
-	private String PhoneNo;
-	private String ExchangeNo;
-	private String MobileNo;
 
-	// 파라미터 검증, @가 들어간 이메일 형식이어야함
-	@Email
-	@ApiModelProperty(name = "이메일", value = "test@test.com")
-	private String eMail;
-	private String PostNo;
-	private String Addr1;
-	private String Addr2;
-
-	// 파라미터 검증, 현재보다 과거여야함
-	// 이밖에 다양한 검증이 있음
-	@Past
-	private Date BirthDay;
 }
